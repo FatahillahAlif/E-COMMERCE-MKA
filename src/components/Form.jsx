f6592c07bacc83a4ac50e2ead3940874d7ea627b
FatahillahAlif <11211039@student.itk.ac.id>
2024-07-29 11:17:41 +0800

    Navbar dan Login

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,32 +1,38 @@
 import React from 'react'
-import { Button, Checkbox, Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Checkbox, Label, TextInput } from "flowbite-react";
 
 function FormComp() {
   return (
-    <form className="flex max-w-md  p-10 border border-black flex-col gap-4 rounded-xl w-96">
-      <div>
+    <form className="relative flex h-[327px] w-[483px] pb-6 justify-center items-center border border-black flex-col gap-4 rounded-lg">
+      <div className="w-96">
         <div className="mb-2 block">
-          <Label htmlFor="email2" value="NIK" />
+          <Label className='font-bold' htmlFor="email2" value="NIK" />
+          <TextInput id="email2" type="number" placeholder="NIK" required shadow />
         </div>
-        <TextInput id="email2" type="email" placeholder="NIK" required shadow />
       </div>
-      <div>
+      <div className="w-96">
         <div className="mb-2 block">
-          <Label htmlFor="password2" value="Password" />
+          <Label className='font-bold' htmlFor="password2" value="Password" />
+          <TextInput id="password2" type="password" placeholder="Password" required shadow />
         </div>
-        <TextInput id="password2" type="password" required shadow />
       </div>
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-2 w-96">
         <Checkbox className='border-black rounded-none bg-transparent' id="agree" />
         <Label htmlFor="agree" className="flex">
           Ingat Saya
         </Label>
       </div>
-      <Button type="submit">LOGIN</Button>
+      <div className='flex justify-center items-center w-96'>
+        <button className='bg-[#1D5C96] w-[184px] h-[31px] flex items-center rounded-lg justify-center text-white font-semibold hover:bg-blue-900' type="submit"> LOGIN</button>
+      </div>
+      <div className='absolute left-0 bottom-0 pl-4 pb-4'>
+        <a href='#' className='underline'>Lupa Password?</a>
+      </div>
+      <div className='absolute right-0 bottom-0 pr-4 pb-4'>
+        <a href='#' className='underline'>Daftar Sekarang</a>
+      </div>
     </form>
   );
 }
 
-
-export default FormComp
+export default FormComp;
